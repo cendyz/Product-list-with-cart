3601a3f3fea844cdb932b4e59f5fe0541e58616c
cendyz <arek57823@gmail.com>
2024-10-14 10:32:07 +0200

    html update

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,41 @@ const foodImage = document.querySelectorAll(".main__box-container-picture-img");
 const minusIcon = document.querySelectorAll(".minus");
 const plusIcon = document.querySelectorAll(".plus");
 const quanityNumber = document.querySelectorAll('[data-setting="quanity"]');
+const cartBox = document.querySelector(".main__summary");
 const cartQuanity = document.querySelector(".main__summary-title span");
 const summaryImg = document.querySelector(".main__summary-img");
 const summaryText = document.querySelector(".main__summary-text");
+const netPrice = document.querySelectorAll(".main__box-text-price");
 const foodName = document.querySelectorAll(".main__box-text-name");
-const cartFoodName = document.querySelector(".main__summary-item-title");
-const cartFoodQuantity = document.querySelector(".main__summary-item-amount-quantity");
+const foodOnePrice = document.querySelectorAll(".main__summary-item-amount-one");
+
+
+
 const food = [0, 0];
 
 const selectQuanity = e => {
 	const index = Array.from(addToCartBtn).indexOf(e.target);
+	const div = document.createElement("div");
+	div.classList.add("main__summary-item");
 	selectContainer[index].style.display = "flex";
 	addToCartBtn[index].style.display = "none";
 	foodImage[index].style.border = "2.2px solid hsl(14, 86%, 42%)";
 	food[index] = 1;
 	quanityNumber[index].textContent = food[index];
-	cartFoodName.textContent = `${foodName[index].textContent}`;
+
+	div.innerHTML = `<h3 class="main__summary-item-title">${foodName[index].textContent}</h3>
+					<div class="main__summary-item-amount">
+						<p class="main__summary-item-amount-quantity">${food[index]}x</p>
+						<p class="main__summary-item-amount-one">@${netPrice[index].textContent}</p>
+						<p class="main__summary-item-amount-bulk">$1.11</p>
+					</div>
+					<button class="main__summary-item-btn">
+						<img src="../src/img/icon-remove-item.svg" alt="X icon" class="main__summary-item-btn-img" />
+					</button>`;
+
+	cartBox.append(div);
 	checkCartQuanity();
+	refreshItems()
 };
 
 const decreaseQuanity = e => {
@@ -35,12 +53,12 @@ const decreaseQuanity = e => {
 	}
 };
 
-const increaseQuanity = e => {
+const increaseQuanity = (e) => {
 	const index = Array.from(plusIcon).indexOf(e.target);
 	food[index]++;
 	quanityNumber[index].textContent = food[index];
-	cartFoodName.textContent = `${foodName[index].textContent}`;
 	checkCartQuanity();
+
 };
 
 const checkCartQuanity = () => {
@@ -58,6 +76,13 @@ const handleOrder = () => {
 const checkCartName = e => {
 	const index = Array.from(food).indexOf(e.target);
 };
+
+const refreshItems = () => {
+	const foodOneQuantity = document.querySelectorAll(".main__summary-item-amount-quantity");
+	const foodOnePrice = document.querySelectorAll(".main__summary-item-amount-one");
+
+	
+}
 
 addToCartBtn.forEach(btn => {
 	btn.addEventListener("click", selectQuanity);
